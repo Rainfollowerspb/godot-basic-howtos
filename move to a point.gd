d74b57fbc643556108959117645572d05ee4e946
Rainfollowerspb <k_belyakov@proton.me>
2025-04-26 21:03:09 +0200

    basic movement

---
+++ move to a point.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+const SPEED = 300.0
+
+var Destination : Vector2 # VECTOR from position to destination
+
+
+func _ready() -> void:
+	Destination = Vector2.ZERO
+
+
+func _process(delta: float) -> void:
+	if Destination:
+		position += move(Destination, delta)
+
+
+func move(destination: Vector2, delta):
+	var increment = destination.normalized() * SPEED * delta
+	if increment.abs() >= destination.abs():
+		increment = destination
+	print(increment)
+	Destination = destination - increment
+	return increment
